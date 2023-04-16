429633572d820c1c08aa4f469f9063f0536cf9d1
snow <vadikking3@gmail.com>
2023-04-17 02:18:14 +0300

    Split index to django code & relocation of files

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -137,3 +137,4 @@ DATABASES = {
         'PORT': '1433',
     }
 }
+'''
